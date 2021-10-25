4a8eafbc48bd24d50958b31db38c0ef159333211
tuan <tuanphan204@gmail.com>
2021-10-25 10:42:46 +0700

    first

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# jstutorial
+# This is guideline to run our project
+
+## Our project is demo about website running
+
+### Technical stacks
+- HTML5
+- CSS3
+- SASS
+- JS
---
+++ b1/assigts/javascript/myfile.js
@@ -0,0 +1,13 @@
+
+let number = prompt("Please input a number");
+
+document.write (typeof number)
+
+
+// ex if else
+// if (typeof number != "number") {
+//   alert("Please input a number!")
+// } else if (number < 0) {
+//   alert("Please input a positive number!")
+// } else if (number >= 0) alert("Congratulations! You did it!")
+
