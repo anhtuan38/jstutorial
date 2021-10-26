a7a03777ebca4187c023275cc8fe7634448bce30
tuan <tuanphan204@gmail.com>
2021-10-26 09:58:27 +0700

    Update Ex

--- Practice4/assigts/javascript/myfile.js
+++ Practice4/assigts/javascript/myfile.js
@@ -1,70 +1,15 @@
-let aDay = prompt("Please input a number from 1 to 7")
+let index;
+do
+  index = prompt("Please input a number from 1 to 7")
+while (index < 1 || index > 7)
+let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-switch (aDay) {
-  case "1":
-    alert("Sunday");
-    break;
-  case "2":
-    alert("Monday");
-    break;
-  case "3":
-    alert("Tuesday");
-    break;
-  case "4":
-    alert("Wednesday");
-    break;
-  case "5":
-    alert("Thursday");
-    break;
-  case "6":
-    alert("Friday");
-    break;
-  case "7":
-    alert("Saturday");
-    break;
-  default:
-    alert("Please input a number from 1 to 7 !!!")
-}
+alert(day[index - 1]);
 
-let aMonth = prompt("Please input a number from 1 to 12")
-
-switch (aMonth) {
-  case "1":
-    alert("January");
-    break;
-  case "2":
-    alert("February");
-    break;
-  case "3":
-    alert("March");
-    break;
-  case "4":
-    alert("April");
-    break;
-  case "5":
-    alert("May");
-    break;
-  case "6":
-    alert("June");
-    break;
-  case "7":
-    alert("July");
-    break;
-  case "8":
-    alert("August");
-    break;
-  case "9":
-    alert("September");
-    break;
-  case "10":
-    alert("October");
-    break;
-  case "11":
-    alert("November");
-    break;
-  case "12":
-    alert("December");
-    break;
-  default:
-    alert("Please input a number from 1 to 12 !!!")
-}
+// Month
+let index2;
+let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+do
+  index2 = prompt("Please input a number from 1 to 12")
+while (index2 < 1 || index2 > 12)
+alert(month[index2 - 1]);
--- Practice8/assigts/javascript/myfile.js
+++ Practice8/assigts/javascript/myfile.js
@@ -1,14 +1,23 @@
 //Create 1 prompt to ask User to input a number that is larger than or equal 3.
+let firstName
 do
-  let firstName = prompt("Please input First Name larger than or equal 3 characters");
+  firstName = prompt("Please input First Name larger than or equal 3 characters");
 while (firstName.length < 3)
+let lastName
 do
-  let lastName = prompt("Please input Last Name larger than or equal 3 characters");
+  lastName = prompt("Please input Last Name larger than or equal 3 characters");
 while (lastName.length < 3)
 
 let genderge = prompt("Please input Genderge");
 let school = prompt("Please input School");
 
+let Address
 do
-  let address = prompt("Please input Address not exceed 40 characters");
-while (address.length > 40)
+  Address = prompt("Please input Address not exceed 40 characters");
+while (Address.length > 40)
+
+document.write("First Name: ",firstName ,"<br>");
+document.write("Last Name: ",lastName ,"<br>");
+document.write("Genderge: ",genderge ,"<br>");
+document.write("School: ",school ,"<br>");
+document.write("Address: ",Address ,"<br>");
