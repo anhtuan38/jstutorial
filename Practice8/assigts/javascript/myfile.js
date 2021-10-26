6a5c30ecea177cc146823f7a5eb34e1685ecb9cf
tuan <tuanphan204@gmail.com>
2021-10-26 11:14:40 +0700

    Update draw table

--- Practice8/assigts/javascript/myfile.js
+++ Practice8/assigts/javascript/myfile.js
@@ -8,6 +8,7 @@ do
   lastName = prompt("Please input Last Name larger than or equal 3 characters");
 while (lastName.length < 3)
 
+let age = prompt("Please input Age:");
 let genderge = prompt("Please input Genderge");
 let school = prompt("Please input School");
 
@@ -16,8 +17,32 @@ do
   Address = prompt("Please input Address not exceed 40 characters");
 while (Address.length > 40)
 
-document.write("First Name: ",firstName ,"<br>");
-document.write("Last Name: ",lastName ,"<br>");
-document.write("Genderge: ",genderge ,"<br>");
-document.write("School: ",school ,"<br>");
-document.write("Address: ",Address ,"<br>");
+
+document.write(
+  `<table>
+    <tr>
+      <th>First name</th>
+      <td>${firstName}</td>
+    </tr>
+    <tr>
+      <th>Last name</th>
+      <td>${lastName}</td>
+    </tr>
+    <tr>
+      <th>Age</th>
+      <td>${age}</td>
+    </tr>
+    <tr>
+      <th>Gender</th>
+      <td>${genderge}</td>
+    </tr>
+    <tr>
+      <th>School</th>
+    <td>${school}</td>
+    </tr>
+    <tr>
+      <th>Address</th>
+      <td>${Address}</td>
+    </tr>
+  </table>`
+)
