507ab6de923f6761e4006cd7487a4001879809db
tuan <tuanphan204@gmail.com>
2021-11-13 22:44:25 +0700

    add file funtion delete 1 Row

--- src/js/deleteRow.js
+++ src/js/deleteRow.js
@@ -1,8 +1,17 @@
 // function delete Row
-const confirmDelete = (index) => {
+const confirmDelRow = (index) => {
   const isDel = confirm("Do you want to delete this row?");
   if (isDel) {
     const indexRowDel = $(`row${index}`);
+
+    // remove element from Array Data
     indexRowDel.remove();
+    personArr.splice(index - 1, 1);
+
+    // creat New Table
+    // console.log(personArr);
+    $("tableData").remove();
+    const newTb = creatTable(personArr);
+    $("data").append(newTb);
   }
 };
