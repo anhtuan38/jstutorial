a3bcd0a546970be626e7bac2cf53c190c998cdd0
tuan <tuanphan204@gmail.com>
2021-11-16 00:13:58 +0700

    add file funtion Search Person

--- src/js/searchPerson.js
+++ src/js/searchPerson.js
@@ -6,14 +6,28 @@ const searchPerson = () => {
   let searchText = textSearchInput.value;
 
   let personSearch = [];
-  personArr.forEach((elemnent) => {
-    if (elemnent[searchValue].indexOf(searchText) !== -1) {
-      personSearch.push(elemnent);
-    }
-  });
+  if (searchValue === "BMI" && isNaN(searchValue)) {
+    personArr.forEach((elemnent) => {
+      if (elemnent[searchValue] <= searchText) {
+        personSearch.push(elemnent);
+      }
+    });
+  } else {
+    personArr.forEach((elemnent) => {
+      if (elemnent[searchValue].indexOf(searchText) !== -1) {
+        personSearch.push(elemnent);
+      }
+    });
+  }
   console.log(personSearch);
   //reload table
-  $("tableData").remove();
-  let newTable = createTable(personSearch);
-  $("data").append(newTable);
+  if (personSearch.length === 0) {
+    $("msgSearch").innerHTML =
+      '<span class="text-danger">There are no results matching your search</span>';
+  } else {
+    $("msgSearch").innerHTML = "";
+    $("tableData").remove();
+    let newTable = createTable(personSearch);
+    $("data").append(newTable);
+  }
 };
